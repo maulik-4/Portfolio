d05c16334ad130649eb08e761473bdce117e8ce9
maulik-4 <maulikvijay4@gmail.com>
2025-02-12 18:55:38 +0530

    secondissue

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -1,9 +1,8 @@
 import React, { useEffect } from "react";
-import Lenis from "@studio-freight/lenis";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import "./Project.css";
-import MouseFollower from '../../Components/Mousefollower/MouseFollower';
+import MouseFollower from '../../Components/MouseFollower/MouseFollower';
 
 
 import project1 from "../../assets/projects/project1.png"
