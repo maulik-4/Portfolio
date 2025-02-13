2baafce5885de1183da79fd3a78c0db30d40d8d5
maulik-4 <maulikvijay4@gmail.com>
2025-02-13 10:01:35 +0530

    secondissue5

---
+++ src/Components/MouseFollower/MouseFollower.jsx
@@ -0,0 +1,43 @@
+import React, { useRef, useEffect } from "react";
+
+const MouseFollower = ({ xscale = 1, yscale = 1 }) => {
+  const circleRef = useRef(null);
+
+  useEffect(() => {
+    const handleMouseMove = (event) => {
+      if (circleRef.current) {
+        circleRef.current.style.transform = `translate(${event.clientX}px, ${event.clientY}px) scale(${xscale}, ${yscale})`;
+      }
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+  
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, [xscale, yscale]);
+
+  return (
+    <div
+      id="minicircle"
+      ref={circleRef}
+      style={{
+        position: "fixed", 
+        top: 0,
+        left: 0,
+        width: "50px",
+        height: "50px",
+        zIndex: 9999, 
+        backgroundColor: "white",
+        borderRadius: "50%",
+        mixBlendMode: "difference",
+        pointerEvents: "none", 
+        
+        transition: "all cubic-bezier(0.19, 1, 0.22, 1) 1s",
+      }}
+    ></div>
+  );
+};
+
+export default MouseFollower;
